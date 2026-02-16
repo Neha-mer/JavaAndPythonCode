8ed3300b0e6f8ef151bccd07a647eaa11ff6abed
Neha Mer <merneha11@gmail.com>
2026-02-16 17:34:21 +0400

    merge two sorted array

--- src/CodingQuesnthirty/MergeTwoSortedArrayList.java
+++ src/CodingQuesnthirty/MergeTwoSortedArrayList.java
@@ -3,10 +3,11 @@ package CodingQuesnthirty;
 import java.util.ArrayList;
 
 public class MergeTwoSortedArrayList {
-
     public static void main(String[] args) {
 
-        ArrayList<Integer> res=new ArrayList<>();
+
+        ArrayList<Integer> res = new ArrayList<>();
+
         ArrayList<Integer> list1 = new ArrayList<>();
         list1.add(1);
         list1.add(3);
@@ -16,34 +17,30 @@ public class MergeTwoSortedArrayList {
         list2.add(2);
         list2.add(4);
         list2.add(6);
-int i=0;int j=0;
 
-        while(i<list1.size() && j<list2.size()){
+        int i = 0, j = 0;
 
-            if(list1.get(i)<list2.get(j)){
+        while (i < list1.size() && j < list2.size()) {
+
+            if (list1.get(i) <= list2.get(j)) {
                 res.add(list1.get(i));
                 i++;
-
-
-            }
-            else if(list1.get(i)>list2.get(j)){
+            } else {
                 res.add(list2.get(j));
                 j++;
             }
-
-
         }
 
-        if(j<list1.size()){
-            res.add(list1.get(j));
-        i++;
+        while (i < list1.size()) {
+            res.add(list1.get(i));
+            i++;
         }
 
-        if(i<list2.size()){
-            res.add(list2.get(i));
+        while (j < list2.size()) {
+            res.add(list2.get(j));
             j++;
-
         }
+
         System.out.println(res);
     }
 }
